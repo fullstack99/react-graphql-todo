c036c6eebcac921a4397b2356e29ff48a0bb16a7
fullstack99 <success1989127@gmail.com>
2021-11-30 19:58:40 -0600

    used react, usecontext, graphql, apollo and implemented unit test

---
+++ src/components/CharactersTable/columns.tsx
@@ -0,0 +1,82 @@
+import { GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
+import { IFavorites } from '../../services/favorites.service';
+import { ReactComponent as HeartIcon } from '../../assets/glyphs/glyph_heart_16.svg';
+import { ReactComponent as HeartIconFilled } from '../../assets/glyphs/glyph_heart_fill_16.svg';
+import * as styles from './characters-table.styles';
+
+export interface IColumnsBuilder {
+  onFavorited: Function;
+  favorites: IFavorites;
+}
+
+export const buildColumns = (props: IColumnsBuilder) => {
+  const favorHandler = (params: GridRenderCellParams) => () => {
+    props.onFavorited(params);
+  }
+
+  const columns: GridColDef[] = [
+    {
+      field: 'id',
+      sortable: false,
+      renderHeader: () => (
+        <HeartIconFilled />
+      ),
+      renderCell: (params: GridRenderCellParams) => {
+        if (props.favorites.cache[params.row.id]) {
+          return (
+            <styles.HeartIconButton aria-label="favorite" onClick={favorHandler(params)}>
+              <HeartIconFilled fill="red" />
+            </styles.HeartIconButton>
+          );
+        }
+
+        return (
+          <styles.HeartIconButton aria-label="favorite" onClick={favorHandler(params)}>
+            <HeartIcon />
+          </styles.HeartIconButton>
+        );
+      },
+    },
+    {
+      field: 'name',
+      headerName: 'Name',
+      minWidth: 200,
+      sortable: true,
+      flex: 1,
+    },
+    {
+      field: 'birthYear',
+      headerName: 'Birth Year',
+      flex: 1,
+      sortable: false,
+    },
+    {
+      field: 'gender',
+      headerName: 'Gender',
+      flex: 1,
+      sortable: false,
+    },
+    {
+      field: 'homeworld',
+      headerName: 'Home World',
+      sortable: false,
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => {
+        if (!params.row.homeworld) return;
+        return params.row.homeworld.name;
+      },
+    },
+    {
+      field: 'species',
+      headerName: 'Species',
+      sortable: false,
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => {
+        if (!params.row.species) return;
+        return params.row.species.name;
+      },
+    },
+  ];
+
+  return columns;
+}
---
+++ src/pages/Home/home.container.tsx
@@ -0,0 +1,71 @@
+import React, { useMemo, useState, useEffect } from 'react';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+
+
+import FavoritesAmountIndicator from '../../components/FavoritesAmountIndicator/favorites-amount-indicator.component';
+import CharactersTable from '../../components/CharactersTable/characters-table.component';
+import FavoritesContext from '../../contexts/favorites.context';
+import FavoritesService from '../../services/favorites.service';
+import * as styles from './home.styles';
+
+const HomeContainer = () => {
+  const [state, setState] = useState({ totalFans: 0, maleFans: 0, femaleFans: 0, otherFans: 0 });
+
+  const providerValue = useMemo(() => {
+    return {
+      updateStatistics: () => {
+        const femaleFans = FavoritesService.countByGender('female');
+        const maleFans = FavoritesService.countByGender('male');
+        const otherFans = FavoritesService.countByGender((gender: string) => !['male', 'female'].includes(gender));
+
+        setState({
+          maleFans,
+          femaleFans,
+          otherFans,
+          totalFans: maleFans + femaleFans + otherFans,
+        });
+      },
+    }
+  }, []);
+
+  useEffect(() => {
+    providerValue.updateStatistics();
+  }, [providerValue]);
+
+  const clearHandler = () => {
+    FavoritesService.clear();
+    providerValue.updateStatistics();
+  }
+
+  return (
+    <Container fixed>
+      <Grid container spacing={2}>
+        <FavoritesContext.Provider value={providerValue}>
+          <Grid item sm={12}>
+            <styles.HeaderBox>
+              <h1>Fans</h1>
+              <styles.ClearButton variant="outlined" color="error" onClick={clearHandler}>
+                Clear fans
+              </styles.ClearButton>
+            </styles.HeaderBox>
+          </Grid>
+          <Grid item sm={4}>
+            <FavoritesAmountIndicator label="Female Fans" value={state.femaleFans} />
+          </Grid>
+          <Grid item sm={4}>
+            <FavoritesAmountIndicator label="Male Fans" value={state.maleFans} />
+          </Grid>
+          <Grid item sm={4}>
+            <FavoritesAmountIndicator label="Others" value={state.otherFans} />
+          </Grid>
+          <Grid item sm={12}>
+            <CharactersTable totalFans={state.totalFans} />
+          </Grid>
+        </FavoritesContext.Provider>
+      </Grid>
+    </Container>
+  );
+}
+
+export default HomeContainer;
